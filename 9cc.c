9c9b33616f89d7039b3c1e9ba94f502a277421e2
eloyrobillard <eloy.robillard@gmail.com>
2021-12-22 18:31:07 +0100

    ✨ feat(compiler): add 加減算

--- 9cc.c
+++ 9cc.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
+void operation(char operator, long num)
+{
+  switch (operator)
+  {
+  case '+':
+    printf("\tadd rax, %ld\n", num);
+    break;
+  case '-':
+    printf("\tsub rax, %ld\n", num);
+    break;
+  }
+}
+
+int generateKeisan(char *input)
+{
+  printf("\tmov rax, %ld\n", strtol(input, &input, 10));
+  while (*input)
+  {
+    char operator= * input;
+    if (operator != '+' && operator != '-') {
+      fprintf(stderr, "予期しない文字です: '%c'\n", operator);
+      return 1;
+    }
+    input++;
+    operation(operator, strtol(input, &input, 10));
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
@@ -11,7 +43,8 @@ int main(int argc, char **argv) {
   printf(".globl main\n");
   printf("\n");
   printf("main:\n");
-  printf("\tmov rax, %d\n", atoi(argv[1]));
+  if (generateKeisan(argv[1]))
+    return 1;
   printf("\tret\n");
   return 0;
 }
