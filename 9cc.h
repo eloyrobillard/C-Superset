f1cf094fcca802909b061950652a7c6cf6076910
eloyrobillard <eloy.robillard@gmail.com>
2022-01-06 17:29:04 +0100

    ✨ feat(grammar): add {} to tokenizer

--- 9cc.h
+++ 9cc.h
@@ -10,7 +10,9 @@
 
 /*
  * program    = stmt*
- * stmt       = "return"? expr ";"
+ * stmt       = expr ";"
+ *            | "{" stmt* "}"
+ *            | "return" expr ";"
  *            | "if" "(" expr ")" stmt ("else" stmt)?
  *            | "for" "(" expr? ";" expr? ";" expr? ")" stmt
  *            | "while" "(" expr ")" stmt
