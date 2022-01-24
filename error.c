0e84515bf775208b88bad50bb379429b3bf59843
eloyrobillard <eloy.robillard@gmail.com>
2022-01-24 08:10:16 +0100

    ✨ feat(error): warning,-atを追加

--- error.c
+++ error.c
@@ -26,9 +26,41 @@ void error_at(char *loc, const char *fmt, ...)
     line_start--;
   int spaces = loc - line_start - 1;
   if (spaces)
-  fprintf(stderr, "%*s", spaces, " "); // pos個の空白を出力
+    fprintf(stderr, "%*s", spaces, " "); // pos個の空白を出力
   fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
+}
+
+void warning(char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  return;
+}
+
+void warning_at(char *loc, const char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  char *lf = loc;
+  while (*lf != '\n' && *lf != EOF)
+    lf++;
+  int off = lf - usr_in;
+  fprintf(stderr, "%.*s\n", off, usr_in);
+
+  char *line_start = loc;
+  while (*line_start != '\n' && line_start >= usr_in)
+    line_start--;
+  int spaces = loc - line_start - 1;
+  if (spaces)
+    fprintf(stderr, "%*s", spaces, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  return;
 }
