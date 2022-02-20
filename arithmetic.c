e60c77f4075917533eb5b65caee2207dc03e10c2
eloyrobillard <eloy.robillard@gmail.com>
2022-02-20 11:22:04 +0100

    🐞 fix(arithmetic): handle_subにis_ptr_or_arrayを使う

--- arithmetic.c
+++ arithmetic.c
@@ -8,18 +8,25 @@ Node *handle_add(Node *node) {
     return new_node(
         ND_ADD, node,
         new_node(ND_MUL, new_node_num(type_size(node->type->ptr_to)), rhs));
-  else if (node->kind == ND_ADDR && node->rhs->type && node->rhs->type->ty == ARRAY)
-    return new_node(
-        ND_ADD, node,
-        new_node(ND_MUL, new_node_num(type_size(node->rhs->type->ptr_to)), rhs));
+  else if (node->kind == ND_ADDR && node->rhs->type &&
+           node->rhs->type->ty == ARRAY)
+    return new_node(ND_ADD, node,
+                    new_node(ND_MUL,
+                             new_node_num(type_size(node->rhs->type->ptr_to)),
+                             rhs));
   else
     return new_node(ND_ADD, node, rhs);
 }
 
+bool is_ptr_or_array(Node *node) {
+  return (node->type && (node->type->ty == PTR || node->type->ty == ARRAY)) ||
+         (node->kind == ND_ADDR && node->rhs->type &&
+             node->rhs->type->ty == ARRAY);
+}
+
 Node *handle_sub(Node *node) {
   Node *rhs = mul();
-  if (node->type && (node->type->ty == PTR || node->type->ty == ARRAY) &&
-      rhs->type && (rhs->type->ty == PTR || rhs->type->ty == ARRAY))
+  if (is_ptr_or_array(node) && is_ptr_or_array(rhs))
     return new_node(ND_DIV, new_node(ND_SUB, node, rhs),
                     new_node_num(type_size(node->type->ptr_to)));
   else if ((node->type && (node->type->ty == PTR || node->type->ty == ARRAY)) ||
